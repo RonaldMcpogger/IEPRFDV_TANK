f3793aed47e28f525a8b856500f438c7d0a8940d
RonaldMcpogger <rameses_amar@dlsu.edu.ph>
2026-01-27 11:35:58 +0800

    setting up basic control movement to tank- will be subjected to change

---
+++ Assets/Scripts/Controllers/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 dir;
+    public float speed = 5f;
+    public InputActionReference moveAction;
+    [SerializeField] private Rigidbody2D rb;
+
+    void Start()
+    {
+        
+        this.rb = this.transform.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir = moveAction.action.ReadValue<Vector2>();
+        Debug.Log(dir);
+        rb.AddForce(dir * speed);
+    }
+}
