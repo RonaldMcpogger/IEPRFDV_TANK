af8ace454c8febb59993ff1bad494a22d9fee2f8
Za13th <156149739+Za13th@users.noreply.github.com>
2026-01-30 22:19:14 +0800

    Improved Tank Movement
    - Made it so that the tank doesn't slide as much after the player stops their inputs.
    - Added the tanks speed now has a limit.
    
    To be added: Halving the tanks X and Y movement speed when two non-opposite direction buttons are pressed (Ex: Up and Left, Down and Right) so that it doesn't move twice as fast diagonally.

--- Assets/Scripts/Controllers/PlayerMovement.cs
+++ Assets/Scripts/Controllers/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
@@ -5,8 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector2 dir;
     public float speed = 5f;
+    public float maxSpeed = 15f;
     public InputActionReference moveAction;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private bool slide = false;
+
+    private int countdown = 0;
+    [SerializeField] private int maxCountdown = 2; //in seconds
 
     void Start()
     {
@@ -20,6 +26,27 @@ public class PlayerMovement : MonoBehaviour
         dir = moveAction.action.ReadValue<Vector2>();
         Debug.Log(dir);
         rb.AddForce(dir * speed);
+
+        if(rb.linearVelocity.sqrMagnitude > maxSpeed) //limits the tanks speed
+        {
+            rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+        }
+
+        if(dir == Vector2.zero && slide == false) //slows down tank after player stops their inputs
+        {
+            rb.linearVelocity = rb.linearVelocity.normalized;
+        }
+
+        if (rb.linearVelocity.sqrMagnitude == 1) //stops the tank after "maxCountdown" seconds have passed
+        {
+            countdown++;
+            if (countdown >= maxCountdown * 60)
+            {
+                rb.linearVelocity *= 0;
+            }
+        }
+        else
+            countdown = 0;
     }
    
 }
