d580e7c5dd4ddd89600ee3560fa8ad3902b8cc42
RonaldMcpogger <rameses_amar@dlsu.edu.ph>
2026-03-17 12:33:34 +0800

    pausescreen for keyboard made

--- Assets/Scripts/PauseScreen.cs
+++ Assets/Scripts/PauseScreen.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PauseScreen : MonoBehaviour
@@ -10,6 +11,10 @@ public class PauseScreen : MonoBehaviour
     bool canPause ;
     [SerializeField] GameObject pauseMenu;
     [SerializeField] List<GameObject> boxes;
+    [SerializeField] InputActionAsset controls1;
+    [SerializeField] InputActionAsset controls2;
+
+
     bool pressed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -24,6 +29,35 @@ public class PauseScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (controls1.FindActionMap("Player1TankControl").FindAction("Start").WasPressedThisFrame() && canPause)
+        {
+            paused = !paused;
+            
+        }
+        else if (controls2.FindActionMap("Player2TankControl").FindAction("Start").WasPressedThisFrame() && canPause)
+        {
+            paused = !paused;
+           
+        }
+
+        if (paused)
+        {
+            Time.timeScale = 0f;
+            pauseMenu.SetActive(true);
+
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            pauseMenu.SetActive(false);
+        }
+    }
+    public void Resume()
+    {
+        paused = false;
+    }
+    public void Quit()
+    {
+       SceneManager.LoadScene("TitleScreen");
     }
 }
